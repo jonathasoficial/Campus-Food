3367abb8112ef726ea7e396f664ec189c938d9bc
setsuna <jonathasoficial1515@gmail.com>
2025-12-21 18:41:20 -0300

    chore: configura environment e autoload do projeto

---
+++ config/environment.rb
@@ -0,0 +1,17 @@
+require_relative "../db/database"
+require_relative "../db/schema"
+
+require_relative "../app/models/cliente"
+require_relative "../app/repositories/cliente_repository"
+
+require_relative "../app/models/funcionario"
+require_relative "../app/repositories/funcionario_repository"
+
+require_relative "../app/models/produto"
+require_relative "../app/repositories/produto_repository"
+
+require_relative "../app/models/venda"
+require_relative "../app/repositories/venda_repository"
+
+require_relative "../app/models/item_venda"
+require_relative "../app/repositories/itemVenda_repository"
