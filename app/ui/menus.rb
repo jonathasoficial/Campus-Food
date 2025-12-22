4e18ea02fa0cb2363fa5e1c79eb90471a3a5e480
setsuna <jonathasoficial1515@gmail.com>
2025-12-22 19:14:43 -0300

    feat(ui): centraliza definição dos menus da aplicação

---
+++ app/ui/menus.rb
@@ -0,0 +1,54 @@
+MENU = <<~MENU
+  ---------------------------------
+          CAMPUS FOOD
+  ---------------------------------
+  1 - Clientes
+  2 - Produtos
+  3 - Funcionários
+  4 - Registrar Venda
+  5 - Relatórios
+  6 - Sair
+  ---------------------------------
+MENU
+
+MENU_CLIENTES = <<~MENU
+  ---------------------------------
+          MENU CLIENTES
+  ---------------------------------
+  1 - Cadastrar clientes
+  2 - Listar clientes
+  3 - voltar
+  ---------------------------------
+MENU
+
+MENU_FUNCIONARIOS = <<~MENU
+  ---------------------------------
+          MENU FUNCIONÁRIOS
+  ---------------------------------
+  1 - Cadastrar funcionários
+  2 - Listar funcionários
+  3 - voltar
+  ---------------------------------
+MENU
+
+MENU_PRODUTOS = <<~MENU
+  ---------------------------------
+          MENU PRODUTOS
+  ---------------------------------
+  1 - Cadastrar produtos
+  2 - Listar produtos
+  3 - voltar
+  ---------------------------------
+MENU
+
+MENU_RELATORIOS = <<~MENU
+  ---------------------------------
+           RELATÓRIOS
+  ---------------------------------
+  1 - Histórico Completo de Vendas
+  2 - Faturamento Diário
+  3 - Produtos Mais Vendidos
+  4 - Clientes Frequentes / Desconto
+  5 - Voltar
+  ---------------------------------
+MENU
